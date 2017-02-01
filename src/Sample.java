3aa70f6884bc130f5c3b87a12fd1747a2e2cc2f6
Vivek22409 <vivekseth09@gmail.com>
2017-02-01 14:58:32 +0530

    Update Sample.java

--- src/Sample.java
+++ src/Sample.java
@@ -4,7 +4,7 @@ class ABC extends Thread {
 	public void run(){
 		for(int i = 0 ; i < 20;i++ ){
 			try {
-				System.out.println("Hello");
+				System.out.println("Hello World");
 				Thread.sleep(500);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
